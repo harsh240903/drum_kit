8b9135434eb7b353f6099195d7f4a9893520e339
harsh240903 <115659533+harsh240903@users.noreply.github.com>
2024-10-24 14:07:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++) {
-
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-
-    var buttonInnerHTML = this.innerHTML;
-
-    makeSound(buttonInnerHTML);
-
-    buttonAnimation(buttonInnerHTML);
-
-  });
-
-}
 
 document.addEventListener("keypress", function(event) {
 
